df2eee63154e6dbcc7bd69a8d36d998683baa26c
Kris Bellemans <kris.bellemans@technomatic.be>
2022-04-08 13:40:53 +0100

    Fetch and log GPS data

---
+++ firmware/experiments/gps.py
@@ -0,0 +1,11 @@
+from gpsdclient import GPSDClient
+import sys
+
+client = GPSDClient(host="127.0.0.1")
+
+for result in client.dict_stream(convert_datetime=True):
+    if result["class"] == "TPV":
+        print("Latitude: %s" % result.get("lat", "n/a"))
+        print("Longitude: %s" % result.get("lon", "n/a"))
+
+        sys.exit(0)
