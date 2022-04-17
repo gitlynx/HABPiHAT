09919209da2f4ecd8793c4b9975054ed6f6b7c87
Jan Cornelis <Jan.Cornelis@gmail.com>
2022-04-17 07:47:31 +0200

    endpoints

--- software/base_station/endpoints.py
+++ software/base_station/endpoints.py
@@ -1,3 +1,6 @@
 endpoints = [
         { 'url': "http://127.0.0.1:8080", 'apikey':"23456"},
+        { 'url': "https://www.highaltitudeballoon.be/siebec/post-data.php", 'apikey':"tPmAT5Ab3j7F9"},
+        { 'url': "https://www.highaltitudeballoon.be/post-data.php", 'apikey':"tPmAT5Ab3j7F9"},
+        { 'url': "https://www.highaltitudeballoon.be/balloonlk/post-data.php", 'apikey':"tPmAT5Ab3j7F9"},
 ]
